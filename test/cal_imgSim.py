9c16e1b2ccb23b7b988933fd21d7f21e6d46f4fd
ZhenqiHe <72725463+zhenqi-he@users.noreply.github.com>
2023-12-10 14:36:03 +0800

    Update cal_imgSim.py

--- test/cal_imgSim.py
+++ test/cal_imgSim.py
@@ -7,44 +7,39 @@ import numpy as np
 
 
 def main():
+    # get path as input
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pred_path', default="/root/autodl-tmp/histology/predicted/aa_cyclegan/test_latest/images")
-    parser.add_argument('--real_path', default="/root/autodl-tmp/histology/predicted/aa_cyclegan/test_latest/images")
+    parser.add_argument('--pred_path', required=True, help="path to reconstructed images")
+    parser.add_argument('--real_path', required=True, help="path to ground truth images")
     args = parser.parse_args()
     
     pred_path = args.pred_path
     real_path = args.real_path
-    
+
+    # get all png images under given paths
     pred_lists = sorted(glob.glob(os.path.join(pred_path,"*.png")))
     gt_lists = sorted(glob.glob(os.path.join(real_path,"*.png")))
-    print("len(gt_lists)",len(gt_lists))
-    print(len(gt_lists),len(pred_lists))
+
+    # they should have the same number of images, otherwise, it is considered as wrong path given
     assert len(gt_lists) == len(pred_lists)
     
-    
-    # evaluation(org_img_path=gt_lists[0], 
-    #        pred_img_path=pred_lists[0], 
-    #        metrics=["ssim", "psnr","fsim","issm","sre","sam","uiq"])
-    
-    # ssim = 0
-    # psnr = 0
-    # fsim = 0
-    # issm = 0
-    # sre = 0
-    # sam = 0
-    # uiq = 0
-    metrics=["ssim", "psnr","fsim","issm","sre","sam","uiq"]
+ 
+    # init all, use a dict to record
+
+    metrics=["ssim", "psnr","fsim"]
     d = {}
     for m in metrics:
         d[m] = 0
     for i,imgP in enumerate(gt_lists):
-        print(i)
+        # print(i)
         e = evaluation(org_img_path=imgP, 
            pred_img_path=pred_lists[i], 
            metrics=metrics)
         for m in metrics:
             d[m] += e[m]
-    
+            
+    # print out results
     for m in metrics:
         print("{} : {}".format(m,d[m]/(i+1)))
         
