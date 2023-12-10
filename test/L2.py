a4a96c480f7689744eda02b872aa4daf1f68e3e1
ZhenqiHe <72725463+zhenqi-he@users.noreply.github.com>
2023-12-10 14:29:59 +0800

    Update L2.py

--- test/L2.py
+++ test/L2.py
@@ -7,45 +7,39 @@ import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
+    # path of prediect images , and ground truth images
     parser.add_argument('--pred_path', default="")
     parser.add_argument('--real_path', default="")
     args = parser.parse_args()
     
     pred_path = args.pred_path
     real_path = args.real_path
-    
+
+    # get all images under paths, sorted is to make the order same
     pred_img_lists = sorted(glob.glob(os.path.join(pred_path,"*.png")))
     real_img_lists = sorted(glob.glob(os.path.join(real_path,"*.png")))
     
-    # l2 = np.sum(np.power((actual_value-predicted_value),2))
-    # print(l2)
-    
-    assert len(pred_img_lists)==len(real_img_lists)
-    
+    #  predicted images and ground truth images should have the same length
+    assert len(pred_img_lists)==len(real_img_lists), 'different number of png images for predicted and ground truth'
+
+    # init the l2 distance
     l2_rgb = 0
-    l2_g = 0
+
     
     for i,img in enumerate(pred_img_lists):
+        # read images
         pred = cv2.imread(img)
-        gray_pred = cv2.cvtColor(pred, cv2.COLOR_BGR2GRAY)
-        
         gt = cv2.imread(real_img_lists[i])
         gt = cv2.resize(gt,(256,256))
-        gt_gray = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
-        gt_gray = cv2.resize(gt_gray,(256,256))
+    
         
-        # l2 = np.sum(np.power((gt-pred),2))
+        # cal the l2 distance
         l2 = np.sum((gt-pred)**2)
-        # print(l2,l2.shape)
-        
-        # l2_gray = np.sum(np.power((gt_gray-gray_pred),2))
-        l2_gray = np.sum((gt_gray-gray_pred)**2)
-        # print(l2_gray,l2.shape)
+
         l2_rgb += l2
-        l2_g += l2_gray
-        
-    print(l2/(i+1))
-    print(l2_g/(i+1))
+    # print out results
+    print(f"L2 distance: {l2/(i+1)}")
+
 
 if __name__=='__main__':
     main()
